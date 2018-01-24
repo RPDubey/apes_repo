1a7872d8da8bf32ce99a2db2e2f8b7ccb645e4a1
rpdubey <radu1571@colorado.edu>
2018-01-24 00:51:56 -0700

    modifications to scripts

--- bin/build_kernel.sh
+++ bin/build_kernel.sh
@@ -9,13 +9,13 @@ echo "Building Kernel" > $LOGPATH/kernel_build.log
 echo "***********************************">>$LOGPATH/kernel_build.log
 
 
-sudo cp /boot/config-$(uname -r) $BUILDPATH/.config >>$LOGPATH/kernel_build.log
+sudo cp /boot/config-$(uname -r) $BUILDPATH/.config &>>$LOGPATH/kernel_build.log
 
-sudo make >>$LOGPATH/kernel_build.log
+sudo make &>>$LOGPATH/kernel_build.log
 
-sudo make modules_install >>$LOGPATH/kernel_build.log
+sudo make modules_install &>>$LOGPATH/kernel_build.log
 
-sudo make install >>$LOGPATH/kernel_build.log
+sudo make install &>>$LOGPATH/kernel_build.log
 
 
 echo "*************** EOF ***************">>$LOGPATH/kernel_build.log
--- bin/sys_info_script.sh
+++ bin/sys_info_script.sh
@@ -3,30 +3,32 @@
 #MYPATH is enviorment variable path to bin. ${MYPATH%%bin} expands to one folder below bin
 LOGPATH=${MYPATH%%bin}/logs/
 
+#sending both stderr and stdout to log file by using "&"
+
 echo "#################################################" >$LOGPATH/system_info.log
-echo "System Specifications" >>$LOGPATH/system_info.log
-echo "#################################################" >>$LOGPATH/system_info.log
+echo "System Specifications" &>>$LOGPATH/system_info.log
+echo "#################################################" &>>$LOGPATH/system_info.log
 
 #User Info
-echo "User Info" >>$LOGPATH/system_info.log
-id -un >>$LOGPATH/system_info.log
-id >>$LOGPATH/system_info.log
+echo "User Info" &>>$LOGPATH/system_info.log
+id -un &>>$LOGPATH/system_info.log
+id &>>$LOGPATH/system_info.log
 echo "*************************************************" >>$LOGPATH/system_info.log
 
 #Operating System Info
 echo "OS Info" >>$LOGPATH/system_info.log
-uname --all >>$LOGPATH/system_info.log
+uname --all &>>$LOGPATH/system_info.log
 echo "*************************************************" >>$LOGPATH/system_info.log
 
 #OS Distribution
 echo "OS Distribution" >>$LOGPATH/system_info.log
-lsb_release -a >>$LOGPATH/system_info.log
+lsb_release -a &>>$LOGPATH/system_info.log
 echo "*************************************************" >>$LOGPATH/system_info.log
 
 
 #OS Version
 echo "OS Version" >>$LOGPATH/system_info.log
-cat /etc/issue.net >>$LOGPATH/system_info.log
+cat /etc/issue.net &>>$LOGPATH/system_info.log
 echo "*************************************************" >>$LOGPATH/system_info.log
 
 
@@ -36,18 +38,18 @@ echo "*************************************************" >>$LOGPATH/system_info.
 #Type of Kernel
 #Kernel Build Time
 echo "Kernel Info" >>$LOGPATH/system_info.log
-cat /proc/version >>$LOGPATH/system_info.log
+cat /proc/version &>>$LOGPATH/system_info.log
 echo "*************************************************" >>$LOGPATH/system_info.log
 
 	
 #System Architecture Info
 echo "System Architecture:" >>$LOGPATH/system_info.log
-lscpu >>$LOGPATH/system_info.log
+lscpu &>>$LOGPATH/system_info.log
 echo "*************************************************" >>$LOGPATH/system_info.log
 
 #Information on File System Memory
 echo "Information on File system and Memory:" >>$LOGPATH/system_info.log
-df -h >>$LOGPATH/system_info.log
+df -h &>>$LOGPATH/system_info.log
 echo "*************************************************" >>$LOGPATH/system_info.log
 
 echo "####################  EOF  ######################" >>$LOGPATH/system_info.log
